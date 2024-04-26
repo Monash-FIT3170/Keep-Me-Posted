0a0d67eb685f9128937ead2ac6fd1ab9ae6e9676
Harrison Lane <hlan0016@student.monash.edu>
2024-04-26 23:50:18 +1000

    fixed issue with not deleting item and converted javascript html content to svelte

--- frontend/src/routes/email/+page.svelte
+++ frontend/src/routes/email/+page.svelte
@@ -2,6 +2,7 @@
     let postRequestString = "http://localhost:8000/emailer/"  // yours may be different, see what link pops up when you run django server, then add /emailer on the end
     let contacts = []
     let contents = ""
+    let emailField = ""
 
     let buttonPressed = () => {
         let data = new FormData()
@@ -14,45 +15,20 @@
     }
     /**
      * This function is used to remove the email from the array and display when the user clicks the delete
-     * @param emailsList
-     * @param id
+     * @param index
      */
-    let remove_email= (emailsList, id)=> {
-        emailsList.splice(id, 1)
-        console.log(emailsList)
+    let remove_email= (index)=> {
+        contacts.splice(index, 1)
+        contacts = contacts
     }
 
     /**
      * add_email function is for displaying the user emails in the display field and for adding the user to the contact array
      */
     let add_email = () =>{
-        //retreiving the email from the input field
-        let emailInputField = document.getElementById("inputEmail");
-        let email_to_display = ""
-        contacts.push(emailInputField.value);
-        
-        let displayField = document.getElementById("display");
-        if (contacts.length>0){
-
-            let htmlDisplay = ``;
-
-            htmlDisplay += `<div style='display: flex; flex-direction: column; align-items: center;'>`;
-            for (let contact_index = 0; contact_index<contacts.length; contact_index++){
-                htmlDisplay += `<div class='row' style="margin:10px;width:250px; height:25px; border:2px solid lightgrey; border-radius:60px; display: flex; justify-content: space-between;">`;
-                email_to_display = contacts[contact_index]
-                htmlDisplay += `<div class='contact'>` + email_to_display + `</div>`;
-                htmlDisplay += `<button on:click={remove_email(contacts, '${contact_index}')} style = "border-radius:100px; cursor:pointer;"><i class = "material-icons" style = "font-size:15px;">remove</i></button>`
-                htmlDisplay +=`</div>`;
-            }
-            htmlDisplay += `</div>`;
-            displayField.innerHTML = htmlDisplay;
-        }
-        else{
-            displayField.innerHTML = `No users added.`;
-            
-        }
-        console.log(contacts)
-
+        contacts.push(emailField)
+        emailField = ""
+        contacts = contacts
     }
 
     
@@ -69,6 +45,15 @@
         text-align: center;
     }
 
+    ul {
+        list-style-type: none;
+    }   
+
+    .email {
+        display: flex;
+        justify-content: center;
+    }
+
 </style>
 
 <br>
@@ -76,16 +61,21 @@
 <div class="heading">Add Receipents<br><br><br><br><br><br>
 <!-- Below is HTML code for the input field and add button-->
 <label for = "email">Enter User Email Address:</label>
-<input id="inputEmail" placeholder = "test@example.com">
+<input placeholder = "test@example.com" bind:value={emailField}>
+
 <!-- on click the add button calls the add_email function-->
 <button on:click={add_email}>Add +</button><br><br><br><br><br>
+
+<!-- loops through each contact and prints -->
+<ul>
+    {#each contacts as email, index (index)}
+    <div class="email"><li>{email}</li> <button on:click={remove_email(index)}>-</button></div>
+    {/each}
+</ul>
+
 
 
 <div id="display" >
 
 </div>
 </div>
-
-
-
-
