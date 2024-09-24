96aa6774045a3c9c0028874b1bf670edbbb8f26a
Harrison Lane <hlan0016@student.monash.edu>
2024-09-24 14:49:01 +1000

    adding request reset password page

---
+++ frontend/src/routes/request_reset/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import reset_password from '../../api-functions/reset_password';
+    import { backendURL } from "../../api-functions/base-URL";
+
+
+    let email = "";
+    let message;
+
+    let requestPasswordReset = async () => {
+        let response = await reset_password(email, backendURL);
+        message = "A password reset link will be sent to your email if you have an account."
+    }
+    
+</script>
+
+<div>
+    <h1>Request Password Reset</h1>
+    <form on:submit|preventDefault={requestPasswordReset}>
+        <label for="email">Enter your email:</label>
+        <input type="email" bind:value={email} required />
+
+        <button type="submit">Reset Password</button>
+    </form>
+
+    {#if message}
+        <p>{message}</p>
+    {/if}
+</div>
