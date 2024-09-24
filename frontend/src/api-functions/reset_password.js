6cf037be92192438f16eb9990e2ef48d3872fc30
Harrison Lane <hlan0016@student.monash.edu>
2024-09-24 14:48:33 +1000

    adding reset password function which calls the backend

---
+++ frontend/src/api-functions/reset_password.js
@@ -0,0 +1,18 @@
+
+export let reset_password = async (email, baseURL) => {
+    const postRequestString = baseURL + "/api/password_reset" 
+
+    const response = await fetch(postRequestString, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+        }),
+    });
+
+    const jsonResponse = await response.json();
+
+    return jsonResponse;
+}
