02987680795b8293248ccf6ea5233489a2ebb095
Harrison Lane <hlan0016@student.monash.edu>
2024-09-27 20:03:27 +1000

    implementing the api functions, email entry, code check, and password change on frontend pages

--- frontend/src/routes/request_reset/+page.svelte
+++ frontend/src/routes/request_reset/+page.svelte
@@ -1,28 +1,53 @@
 <script>
-    import { reset_password } from '../../api-functions/reset_password';
+    import { request_reset_password} from '../../api-functions/request_reset_password';
     import { backendURL } from "../../api-functions/base-URL";
     import { goto } from "$app/navigation";
     import { ResetPasswordStore } from "../../stores/reset-password-store"
 
     let message = ""
+    let codeSent = false
+    let code =  ""
 
     let requestPasswordReset = async () => {
-        let response = await reset_password($ResetPasswordStore.email, backendURL);
+        let response = await request_reset_password($ResetPasswordStore.email, backendURL);
         
         if (response.status === 200) {
-            goto("/reset_password")
+            codeSent = true
+            message = "A code has been sent to your email"
         } else {
             message = "There is no account matching that email"
+        }
+    }
+
+    let checkCode = () => {
+        if (code === atob($ResetPasswordStore.code)) {
+            goto("/reset_password")
+        } else {
+            message = "The code provided is not correct. Please try again."
         }
     }
     
 </script>
 
+<!-- TODO
+Make this look pretty
+Make sure the enter email text box binds to the store - bind:value={ $ResetPasswordStore.email}
+Make sure the reset password button binds to the function - on:click={() => requestPasswordReset()}
+Make sure that the different boxes are rendered based on if the code has been sent - codeSent
+Make sure that the code is binded to the variable - bind:value={ code }
+-->
+
 <div>
+    {#if !codeSent}
     <h3>Enter your email</h3>
     <input type="email" bind:value={ $ResetPasswordStore.email}/>
-    <button on:click={() => requestPasswordReset()}>Reset password</button>
+    <button on:click={() => requestPasswordReset()}>Request Reset</button>
+    {:else} 
+    <h3>Enter the validation code sent to your email</h3>
+    <input bind:value={ code }/>
+    <button on:click={() => checkCode()}>Reset password</button>
+    {/if}
     {#if message}
-        <h3>message</h3>
+        <h3>{message}</h3>
     {/if}
 </div>
--- frontend/src/routes/reset_password/+page.svelte
+++ frontend/src/routes/reset_password/+page.svelte
@@ -1,3 +1,48 @@
+<script>
+    import { reset_password} from '../../api-functions/reset_password';
+    import { backendURL } from "../../api-functions/base-URL";
+    import { ResetPasswordStore } from "../../stores/reset-password-store"
+    import { goto } from "$app/navigation";
+    
+    let password1 = ""
+    let password2 = ""
+    let errorMessage = ""
+
+    let resetPassword = async () => {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+,\-.;:'"<>=?\/\[\]{}|`~])(?=.{8,})[a-zA-Z0-9!@#$%^&*()_+,\-.;:'"<>=?\/\[\]{}|`~]*$/;
+
+        if (!passwordRegex.test(password1)) {
+            errorMessage = "Password must contain at least 8 characters, capital letters, and a special character";
+            return
+        }
+
+        if (password1 !== password2) {
+            errorMessage = "Passwords do not match"
+            return
+        }
+
+        let response = await reset_password($ResetPasswordStore.email, password1, backendURL);
+        
+        if (response.status === 200) {
+            errorMessage = "Password successfully reset"
+            setTimeout(() => {
+                goto("/login")
+            }, 1500)
+        } else {
+            errorMessage = "Password reset failed"
+        }
+    }
+
+</script>
+
 <div>
-    Hello world
+    <h3>Enter your new password</h3>
+    <input type="password" placeholder="••••••••" bind:value={ password1 }/>
+    <h3>Re-enter your new password</h3>
+    <input type="password" placeholder="••••••••" bind:value={ password2 }/>
+
+    <button on:click={() => resetPassword()}>Reset password</button>
+    {#if errorMessage}
+        <h3>{errorMessage}</h3>
+    {/if}
 </div>
