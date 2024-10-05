9f9fdc55d3f74c6044d2c0f4015235cdd30bfd3c
afia-farzana <afar0035@student.monash.edu>
2024-10-05 20:34:23 +1000

    Made small adjustments for view flexibility and edited header.

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -1,9 +1,9 @@
-<!-- Summary generation page
+<!-- Profile Page 
 
-    Page that shows the summary generated
+    Page that shows previously sent summaries 
 
-    Author: Brenda Dang, Diya Ramesh
-    Last Modified: 1/08/2024
+    Author: Jeremy Wang, Afia Farzana
+    Last Modified: 05/10/2024
 -->
 
 <script>
@@ -11,22 +11,22 @@
     import Topbar from "../../components/topbar.svelte";
 </script>
   
-  <body>
+  <body class="p-4 sm:p-8">
     <Topbar />
-    <h1 class="text-4xl font-bold text-center mt-6 mb-4 text-gray-900 dark:text-white">
+    <h1 class="class=text-3xl sm:text-4xl font-bold text-center mt-6 mb-4 text-gray-900 dark:text-white"> 
       Summary History
     </h1>
-    <h2 class="text-lg font-normal text-center mt-4 mb-6 text-gray-700 dark:text-gray-400">
+    <h2 class="text-lg sm:text-xl font-normal text-center mt-4 mb-6 text-gray-700 dark:text-gray-400">
       View your previously generated summaries here
   </h2>
     <!-- Card Container Section -->
-  <div class="flex flex-row gap-4 flex-wrap justify-center">
+  <div class="flex flex-col sm:flex-row gap-4 flex-wrap justify-center">
     <!-- First Card -->
-    <Card class="max-w-sm bg-white shadow-lg rounded-lg p-6" />
+    <Card class="w-full sm:max-w-sm bg-white shadow-lg rounded-lg p-6" />
     
     <!-- Add more cards here -->
-    <Card class="max-w-sm bg-white shadow-lg rounded-lg p-6" />
-    <Card class="max-w-sm bg-white shadow-lg rounded-lg p-6" />
+    <Card class="w-full sm:max-w-sm bg-white shadow-lg rounded-lg p-6" />
+    <Card class="w-full sm:max-w-sm bg-white shadow-lg rounded-lg p-6" />
   </div>
   </body>
   
