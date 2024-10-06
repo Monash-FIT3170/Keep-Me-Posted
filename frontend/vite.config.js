6a08b008023a63ef3b43195a4c84c720ced63e2d
aleu0007 <aleu0007@student.monash.edu>
2024-10-06 22:37:43 +1100

    added environment variables check in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-    server: {
-        fs: {
-            allow: ['/src/']
-        }
+const requiredEnvVars = ['VITE_SECRET_CLIENT_ID', 'VITE_SECRET_CLIENT_SECRET'];
+
+requiredEnvVars.forEach((varName) => {
+    const envValue = import.meta.env[varName];
+    console.log(envValue);
+
+    // check if environment variable is missing
+    if (!envValue || envValue === undefined) {
+        console.error('Missing required frontend environment variable: ${varName}')
     }
-}
-);
+
+    if (envValue === "REPLACE_WITH_API_KEY") {
+        console.error('Environment variable ${varName} is still set to "REPLACE_WITH_API_KEY"');
+    }
+});
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+    return {
+        plugins: [sveltekit()],
+        server: {
+            fs: {
+                allow: ['/src/']
+            }
+        },
+    };
+});
