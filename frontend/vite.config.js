72b79f4ec17ec8390239eb5b619663d6af17ce62
aleu0007 <126241761+aleu0007@users.noreply.github.com>
2024-10-07 12:38:15 +1100

    Feature/86cwmzbyf template env file (#165)
    * npm audit fix
    
    * .env and .env.local files
    
    * removed env from gitignore
    
    * updated .env.local variable names in readme
    
    * added environment variables check in vite config file
    
    * added environment variable checks in backend manage.py
    
    * resolved not being able to read .env.local using https://github.com/vitejs/vite/issues/7430
    
    * removed undefined conditional
    
    * log missing .env variables when running backend's manage.py
    
    * add .env to gitignore so no more env changes will be commited

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-    server: {
-        fs: {
-            allow: ['/src/']
+const requiredEnvVars = ['VITE_SECRET_CLIENT_ID', 'VITE_SECRET_CLIENT_SECRET'];
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+    requiredEnvVars.forEach((varName) => {
+        const envValue = env[varName];
+    
+        // check if environment variable is missing
+        if (!envValue) {
+            console.error(`Missing required frontend environment variable: ${varName}`)
         }
-    }
-}
-);
+    
+        if (envValue === "REPLACE_WITH_API_KEY") {
+            console.error(`Environment variable ${varName} is still set to "REPLACE_WITH_API_KEY"`);
+        }
+    });
+
+    return {
+        plugins: [sveltekit()],
+        server: {
+            fs: {
+                allow: ['/src/']
+            }
+        },
+    };
+});
