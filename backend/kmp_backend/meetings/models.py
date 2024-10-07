72638f9bb2a4191709d787babeb532b327b99986
Rohit Valanki <rval00008@student.monash.edu>
2024-10-07 13:44:38 +1100

    Added api route for frontend to update meeting summaries

--- backend/kmp_backend/meetings/models.py
+++ backend/kmp_backend/meetings/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 class Meeting(models.Model):
     user_email = models.EmailField()
-    meeting_transcript = models.TextField()
+    meeting_transcript = models.TextField(default="")  # Set default to an empty string
     meeting_summary = models.TextField()
-    meeting_recipients = models.JSONField(default=list)  # Set default to an empty list
+    meeting_recipients = models.JSONField(default=list)
     meeting_date = models.DateTimeField()
 
     def __str__(self):
         return self.meeting_transcript
+
