a965bd0d3353c5733b9b18b2123e1746ea0d154b
Parul Garg <pgar0011@student.monash.edu>
2024-10-09 12:44:14 +1100

    added code documentation to the component file

--- frontend/src/components/emailEntry.svelte
+++ frontend/src/components/emailEntry.svelte
@@ -1,4 +1,19 @@
+<!--
+
+	Email Input Box and Add Recipients Button Component
+
+	consists of parts
+	1. the email input field along with the icon
+	2. the add recipients button
+
+	Authors: Harrison Lane
+	Editied by: Parul Garg, Angelina Leung, Maureen Pham
+	Last Modified: 9/10/24 by Parul Garg
+
+-->
+
 <script>
+  // necessary imports
   import Button from "../components/button.svelte";
   import { ContactsStore } from "../stores/contacts-store"
   import AddIconBlue from "../assets/add-icon-blue.png"
@@ -6,10 +21,14 @@
   let emailString = "";
   let emailErrorString = "";
 
+  //logic to check and add the entered email
   let addEmail = () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // checking if entered email string is valid
     if (emailRegex.test(emailString)) {
+      // if valid, add the email to the contacts store
       ContactsStore.update((prev) => {
+        // handling duplicate email entries
         if (prev.includes(emailString)) {
           return prev;
         } else {
@@ -17,7 +36,9 @@
         }
       });
       emailString = "";
-    } else {
+    } 
+    // if email string is not valid
+    else {
       emailErrorString = "Please enter a valid email address.";
       setTimeout(() => {
         emailErrorString = "";
@@ -26,26 +47,33 @@
   };
 </script>
 
+<!-- Input box and the add recipients buttton -->
 <div class="flex justify-center gap-2 px-4">
+  <!-- Input box div -->
     <div class="relative">
+      <!-- Input box -->
       <input
         class="border border-slate-300 pl-9 p-2 rounded-xl w-full sm:w-96 h-full"
         type="email"
         placeholder="johndoe@email.com"
         bind:value={emailString}
       />
+      <!-- mail icon in the input box -->
       <button
             class="absolute cursor-default left-3 top-1/2 transform -translate-y-1/2"
             type="button">
           <img src={mailIcon} id="icon" class="h-4 w-4"/>
       </button>
     </div>
+  
+  <!-- Add recipients button div -->
   <Button
     handleClick={addEmail}
     icon={AddIconBlue}
     text=""
     type="secondary"
   >
+  <!--hiding the text in mobile view-->
     <span class="hidden sm:inline sm:ml-2 text-blue-700">Add Recipient</span>
   </Button>
 </div>
