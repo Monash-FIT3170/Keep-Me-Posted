7616ba1c2bf64241baf824cfd2e6abd8a6dd77de
Brenda Dang <bdan0005@student.monash.edu>
2024-10-10 08:19:18 +1100

    Implement log out pop up

--- frontend/src/components/topbar.svelte
+++ frontend/src/components/topbar.svelte
@@ -8,10 +8,12 @@
   import { getAuth, authStore, clearAuth } from "../stores/auth-store.js";
   import { goto } from "$app/navigation";
   import { resetStores } from "../stores/reset-store.js";
+  import PopUpModal from '../components/popUpModal.svelte';
 
   let showDropdown = false;
   let profileIcons = [profileIcon, profileHoverIcon];
   let currentIconIndex = 0;
+  let popUpModalComponent;  
 
   let email = ""; // Initialize email as an empty string
 
@@ -48,9 +50,28 @@
   function handleSignIn() {
     goto("/login");
   }
+
+  // Function to handle toggling the pop up
+  function toggleLogOut() {
+    popUpModalComponent.togglePopUp();
+    toggleDropdown();
+  }
 </script>
 
 <body>
+  <div class="">
+    <PopUpModal
+    bind:this={popUpModalComponent}
+    type="secondary"
+    header="Are you sure you want to log out?"
+    mainText="Your session will not be saved."
+    firstButtonText="Log Out"
+    secondButtonText="Cancel"
+    firstHandleClick={handleLogout}
+    secondHandleClick={toggleLogOut}
+    width="96"
+    />
+  </div>
   <div class="flex justify-center items-center justify-between border-b h-20">
     <div class="px-8 m">
       <button on:click={() => handleGoHome()}>
@@ -68,14 +89,14 @@
           </button>
 
           {#if showDropdown}
-            <div class="w-fit absolute bg-white shadow right-16 py-2 rounded-lg z-50">
+            <div class="w-fit absolute bg-white shadow right-16 py-2 rounded-lg z-40">
               <div class="text-left w-fit py-2 rounded-lg">
                 <div class="text-gray-500 text-base px-5 pb-2 w-full">
                   {email} <!-- Show the logged-in user's email -->
                 </div>
                 <div class="w-full border-t border-gray-100"></div>
                 <div class="flex w-full float-start items-start justify-start pt-3 pb-1 px-3">
-                  <button class="hover:bg-gray-100 rounded-lg font-sans justify-center items-center flex w-full h-8" on:click={() => handleLogout()}>
+                  <button class="hover:bg-gray-100 rounded-lg font-sans justify-center items-center flex w-full h-8" on:click={popUpModalComponent.togglePopUp()}>
                     <div class="justify-start items-center flex flex-row w-full ms-1 px-1">
                       <img class="h-4" src={logOutIcon} alt="Log Out Icon" />
                       <div class="text-gray-700 text-base px-3">Log Out</div>
