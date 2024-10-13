851fcbed28a58627fdc972c0d0caeba0ce405259
Ahmed Almasry <aalm0027@student.monash.edu>
2024-10-13 21:26:39 +1100

    update loading icon colors

--- frontend/src/components/button.svelte
+++ frontend/src/components/button.svelte
@@ -59,8 +59,8 @@
 
         {#if loading}
             <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-35" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="#7d7d7d" stroke-width="4"></circle>
+                <path class="opacity-35" fill="#6a6a6a" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
             {text}
         {:else}
