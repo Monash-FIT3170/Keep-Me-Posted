0c97f826771ee86aa1acedadc42a58a52c0261d2
Alex Ung <aung0008@student.monash.edu>
2024-10-16 00:49:51 +1100

    removed make migrations

--- run.bat
+++ run.bat
@@ -6,4 +6,4 @@ start cmd /k "cd frontend && npm run dev"
 
 :: Function to start backend server
 echo Starting backend server...
-start cmd /k "cd backend && (if not exist venv (python -m venv venv)) && (call venv\Scripts\activate.bat) && cd kmp_backend && python manage.py makemigrations meetings && python manage.py migrate && python manage.py runserver"
+start cmd /k "cd backend && (if not exist venv (python -m venv venv)) && (call venv\Scripts\activate.bat) && cd kmp_backend && python manage.py migrate && python manage.py runserver"
