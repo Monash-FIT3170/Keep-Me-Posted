c7f663432272ab2f5b726137ca2d7d8cbc716f91
Alex Ung <aung0008@student.monash.edu>
2024-10-16 00:29:10 +1100

    created a set of run files with no installation to speed up local hosting

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Function to start frontend server
+echo Starting frontend server...
+start cmd /k "cd frontend && npm run dev"
+
+:: Function to start backend server
+echo Starting backend server...
+start cmd /k "cd backend && (if not exist venv (python -m venv venv)) && (call venv\Scripts\activate.bat) && cd kmp_backend && python manage.py makemigrations meetings && python manage.py migrate && python manage.py runserver"
